0cd03ea301075baecca7b7822f9b76ebe947a5c9
vijuprakashgithub <inboxtoviju@gmail.com>
2020-07-11 14:03:15 +0530

    This file was created by M. Viju Prakash

---
+++ src/com/viju/Main.java
@@ -0,0 +1,23 @@
+package com.viju;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int number1 = 100500;
+        byte number2 = 100;
+        short number3 = 1500;
+        long number4 = 12345768L;
+        float number5 = 20.545F;
+        double number6 = 23593.54939;
+        char letter = 'V';
+        Boolean bool = true;
+        System.out.println("The provided integer number is "+number1);
+        System.out.println("The provided byte number is "+number2);
+        System.out.println("The provided short number is "+number3);
+        System.out.println("The provided long number is "+number4);
+        System.out.println("The provided float number is "+number5);
+        System.out.println("The provided double number is "+number6);
+        System.out.println("The provided character value is "+letter);
+        System.out.println("The provided boolean value is "+bool);
+    }
+}
